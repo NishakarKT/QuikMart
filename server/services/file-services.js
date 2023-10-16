eeeaa8f5be56b8f7934bd1743b1b04da36842a5c
NishakarKT <nishakarkumar0@outlook.com>
2023-10-16 14:23:32 +0530

    add missing media dir

--- server/services/file-services.js
+++ server/services/file-services.js
@@ -1,8 +1,16 @@
 import multer from "multer";
+import fs from "fs";
+
+const dir = "./media";
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => { cb(null, 'media/') },
-    filename: (req, file, cb) => { cb(null, file.originalname) }
+  destination: (req, file, cb) => {
+    cb(null, "media/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 export const upload = multer({ storage: storage });
